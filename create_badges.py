92c9a3bc8ed6183ae3cc5c6166c7960ae4a38d65
Erik Smistad <ersmistad@gmail.com>
2025-10-16 11:43:11 +0200

    Make all time badge

--- create_badges.py
+++ create_badges.py
@@ -16,22 +16,26 @@ df = pd.DataFrame({'date': pd.to_datetime(list(data.keys())),
                    })
 
 df = df.set_index('date')
+total = df.agg({'downloads': 'sum'})[0]
 end_date = df.index.max()
 start_date = end_date - pd.DateOffset(months=1)
 df_last_month = df[df.index >= start_date]
-total = df_last_month.agg({'downloads': 'sum'})[0]
-print(total)
+total_last_month = df_last_month.agg({'downloads': 'sum'})[0]
 
 
-url = f'https://img.shields.io/badge/Pip_Downloads-{total}_last_month-green?logo=python'
+def create_badge(name:str, url_string:str):
+    url = f'https://img.shields.io/badge/{url_string}'
 
-response = requests.get(url)
+    response = requests.get(url)
 
-filename = 'badges/last_month.svg'
-if response.status_code == 200:
-    with open(filename, 'wb') as file:
-        for chunk in response.iter_content(chunk_size=1024):
-            file.write(chunk)
-    print(f"Image downloaded successfully to {filename}")
-else:
-    print('Failed')
+    filename = f'badges/{name}.svg'
+    if response.status_code == 200:
+        with open(filename, 'wb') as file:
+            for chunk in response.iter_content(chunk_size=1024):
+                file.write(chunk)
+        print(f"Image downloaded successfully to {filename}")
+    else:
+        print('Failed')
+
+create_badge('last_month', f'Pip_Downloads-{total_last_month}_last_month-green?logo=python')
+create_badge('all_time', f'Pip_Downloads-{round(total/1000, 1)}k-green?logo=python')
