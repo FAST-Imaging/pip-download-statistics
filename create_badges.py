ae835ecc46b2ddb8508f2aa262ec61cf05f76856
Erik Smistad <ersmistad@gmail.com>
2025-12-15 08:43:48 +0100

    create_badges use data from all years

--- create_badges.py
+++ create_badges.py
@@ -1,14 +1,14 @@
 import json
-from datetime import datetime
-import json
 import pandas as pd
 import requests
+import os
 
-# Get downloads over last month
-year = str(datetime.now().year)
-
-with open(f'data/{year}.json', 'r') as file:
-    data = json.load(file)
+# Get data for all the years
+data = {}
+for filename in os.listdir('data/'):
+    if '_' not in filename and '.json' in filename:
+        with open(f'data/{filename}', 'r') as file:
+            data.update(json.load(file))
 
 # Per month
 df = pd.DataFrame({'date': pd.to_datetime(list(data.keys())),
@@ -36,6 +36,7 @@ def create_badge(name:str, url_string:str):
         print(f"Image downloaded successfully to {filename}")
     else:
         print('Failed')
+
 
 create_badge('last_month', f'Pip_Downloads-{total_last_month}_last_month-green?logo=python')
 create_badge('all_time', f'Pip_Downloads-{round(total/1000, 1)}k-green?logo=python')
